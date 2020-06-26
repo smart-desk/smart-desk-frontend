938bee0b27cac5b8864400705b66b9fb07e5dae9
Anton Cherepov <chekitdnb@gmail.com>
2020-06-26 08:02:46 +0200

    fixed indentation

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,41 +4,41 @@ import { AppComponent } from './app.component';
 import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
-  let fixture: ComponentFixture<AppComponent>;
-  let component: AppComponent;
-  let element: HTMLElement;
+    let fixture: ComponentFixture<AppComponent>;
+    let component: AppComponent;
+    let element: HTMLElement;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                RouterTestingModule
+            ],
+            declarations: [
+                AppComponent
+            ],
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AppComponent);
-    component = fixture.componentInstance;
-    element = fixture.nativeElement;
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AppComponent);
+        component = fixture.componentInstance;
+        element = fixture.nativeElement;
 
-    fixture.detectChanges();
-  });
+        fixture.detectChanges();
+    });
 
-  it('should create the app', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create the app', () => {
+        expect(component).toBeTruthy();
+    });
 
-  it(`should have as title 'Welcome to magic Smart Desk'`, () => {
-    expect(component.title).toContain('Welcome to magic Smart Desk');
-  });
+    it(`should have as title 'Welcome to magic Smart Desk'`, () => {
+        expect(component.title).toContain('Welcome to magic Smart Desk');
+    });
 
-  it('should render title', () => {
-    const titleDe = fixture.debugElement.query(By.css('[data-test-app-title]'));
+    it('should render title', () => {
+        const titleDe = fixture.debugElement.query(By.css('[data-test-app-title]'));
 
-    expect(titleDe).toBeTruthy('App title should be in the DOM');
-    expect(titleDe.nativeElement.textContent).toContain('Welcome to magic Smart Desk');
-  });
+        expect(titleDe).toBeTruthy('App title should be in the DOM');
+        expect(titleDe.nativeElement.textContent).toContain('Welcome to magic Smart Desk');
+    });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Welcome to magic Smart Desk';
+    title = 'Welcome to magic Smart Desk';
 }
