d514111b478871e56563cb92f8acc6d7a7ad1bb0
Tim Shenin <t.shenin@celonis.com>
2020-07-04 20:36:47 +0200

    Added test files

--- src/app/shared/services/model/model.service.spec.ts
+++ src/app/shared/services/model/model.service.spec.ts
@@ -1,16 +1,65 @@
-import { TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
 import { ModelService } from './model.service';
+import { Model, ModelExtended } from '../../models/models.dto';
+
+let httpClientSpy: {
+    get: jasmine.Spy;
+    post: jasmine.Spy;
+    put: jasmine.Spy;
+    delete: jasmine.Spy;
+};
 
 describe('ModelService', () => {
     let service: ModelService;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
-        service = TestBed.inject(ModelService);
+        httpClientSpy = jasmine.createSpyObj('HttpClient', ['get', 'post', 'put', 'delete']);
+        service = new ModelService(httpClientSpy as any);
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it('should get all models', () => {
+        const expectedModels: Model[] = [
+            { id: '1', name: 'A' },
+            { id: '2', name: 'B' },
+        ];
+
+        httpClientSpy.get.and.returnValue(of(expectedModels));
+
+        service.getModels().subscribe(models => expect(models).toEqual(expectedModels, 'expected models'), fail);
+        expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+    });
+
+    it('should get extended model', () => {
+        const expectedModel: ModelExtended = {
+            id: '1',
+            name: 'Test',
+            sections: [
+                {
+                    id: '1',
+                    model_id: '1',
+                    fields: [
+                        {
+                            id: '1',
+                            type: 'input_text',
+                            section_id: '1',
+                        },
+                        {
+                            id: '2',
+                            type: 'text',
+                            section_id: '1',
+                        },
+                    ],
+                },
+            ],
+        };
+
+        httpClientSpy.get.and.returnValue(of(expectedModel));
+
+        service.getModel('1').subscribe(models => expect(models).toEqual(expectedModel, 'expected model'), fail);
+        expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
     });
 });
--- src/app/shared/services/section/section.service.spec.ts
+++ src/app/shared/services/section/section.service.spec.ts
@@ -1,16 +1,3 @@
-import { TestBed } from '@angular/core/testing';
-
 import { SectionService } from './section.service';
 
-describe('SectionService', () => {
-    let service: SectionService;
-
-    beforeEach(() => {
-        TestBed.configureTestingModule({});
-        service = TestBed.inject(SectionService);
-    });
-
-    it('should be created', () => {
-        expect(service).toBeTruthy();
-    });
-});
+describe('SectionService', () => {});
