3eed86a2168e9581c881e658c5e187d178547ca8
Anton Cherepov <chekitdnb@gmail.com>
2020-07-05 14:22:01 +0200

    Added readme for structure, added core and shared modules (#4)

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,15 @@
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+
+@NgModule({
+    imports: [],
+    exports: [],
+    declarations: [],
+    providers: [],
+})
+export class CoreModule {
+    constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+        if (parentModule) {
+            throw new Error(`CoreModule has already been loaded. Import CoreModule in the AppModule only.`);
+        }
+    }
+}
