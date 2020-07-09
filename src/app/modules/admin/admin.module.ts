900b0314bb39b69af6b9b8c850dd407762f7e0b5
Tim Shenin <t.shenin@celonis.com>
2020-07-10 00:23:10 +0200

    Added form for creating a model

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -9,14 +9,35 @@ import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+import { NzSelectModule } from 'ng-zorro-antd/select';
 import { IconDefinition } from '@ant-design/icons-angular';
 import { ApartmentOutline } from '@ant-design/icons-angular/icons';
 import { ModelsComponent } from './pages/models/models.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 const icons: IconDefinition[] = [ApartmentOutline];
 
 @NgModule({
-    imports: [AdminRoutingModule, CommonModule, NzLayoutModule, NzMenuModule, NzIconModule.forRoot(icons), NzCardModule, NzGridModule],
+    imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        AdminRoutingModule,
+        CommonModule,
+        NzLayoutModule,
+        NzMenuModule,
+        NzIconModule.forRoot(icons),
+        NzCardModule,
+        NzGridModule,
+        NzFormModule,
+        NzInputModule,
+        NzTypographyModule,
+        NzButtonModule,
+        NzSelectModule,
+    ],
     declarations: [AdminComponent, EditModelComponent, CreateModelComponent, ModelsComponent],
 })
 export class AdminModule {}
--- src/app/modules/admin/pages/create-model/create-model.component.ts
+++ src/app/modules/admin/pages/create-model/create-model.component.ts
@@ -1,12 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Form, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ModelService } from '../../../../core/services/model/model.service';
+import { switchMap, take } from 'rxjs/operators';
+import { Model, Section } from '../../../../core/models/models.dto';
+import { SectionService } from '../../../../core/services/section/section.service';
 
 @Component({
     selector: 'app-create-model',
     templateUrl: './create-model.component.html',
     styleUrls: ['./create-model.component.scss'],
 })
-export class CreateModelComponent implements OnInit {
-    constructor() {}
+export class CreateModelComponent {
+    public model: Model;
+    public section: Section;
 
-    ngOnInit(): void {}
+    public currentFieldType: string; // todo enum
+
+    // todo вывести ошибки
+    public modelForm = new FormGroup({
+        name: new FormControl('', Validators.required),
+    });
+
+    // todo вывести ошибки
+    public sectionForm = new FormGroup({
+        fields: new FormArray([]),
+    });
+
+    constructor(private modelsService: ModelService, private sectionService: SectionService) {}
+
+    public createModel() {
+        this.modelsService
+            .createModel({
+                name: this.modelForm.get('name').value,
+            })
+            .pipe(
+                switchMap(model => {
+                    this.model = model;
+                    return this.sectionService.createSection({
+                        model_id: this.model.id,
+                    });
+                }),
+                take(1)
+            )
+            .subscribe(section => {
+                this.section = section;
+            });
+    }
+
+    public getSectionFieldsControls(): FormArray {
+        return this.sectionForm.get('fields') as FormArray;
+    }
+
+    public addFieldToSectionForm() {
+        // todo use this.currentTypeField
+        const fields = this.sectionForm.get('fields') as FormArray;
+        fields.push(new FormControl(''));
+    }
 }
