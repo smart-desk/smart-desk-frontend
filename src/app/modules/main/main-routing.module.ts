2d7cdd48d36d7d648496dcdeddfec3a332a0ce4a
aradionov <aradionov@phoenixit.ru>
2020-08-16 21:34:18 +0700

    add main-router

---
+++ src/app/modules/main/main-routing.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { AdvertComponent } from './pages/advert/advert.component';
+import { AdvertEditComponent } from './pages/advert-edit/advert-edit.component';
+import { AdvertCreateComponent } from './pages/advert-create/advert-create.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: MainComponent,
+        children: [
+            {
+                path: '',
+                component: CategoriesComponent,
+            },
+            {
+                path: 'adverts/create',
+                component: AdvertCreateComponent,
+            },
+            {
+                path: 'adverts/:advert_id/edit',
+                component: AdvertEditComponent,
+            },
+            {
+                path: ':category_id',
+                component: CategoryComponent,
+            },
+            {
+                path: ':category_id/:advert_id',
+                component: AdvertComponent,
+            },
+        ],
+    },
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
+})
+export class MainRoutingModule {}
---
+++ src/app/modules/main/pages/advert-create/advert-create.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-advert-create',
+    templateUrl: './advert-create.component.html',
+    styleUrls: ['./advert-create.component.scss'],
+})
+export class AdvertCreateComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
