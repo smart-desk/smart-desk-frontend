586fb93d03df369d8addc7fd8bb7fafb666788c1
Tim Shenin <t.shenin@celonis.com>
2020-09-04 23:56:59 +0200

    Bypassing advert error

--- src/app/shared/services/advert/advert.service.ts
+++ src/app/shared/services/advert/advert.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Advert } from '../../models/models.dto';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class AdvertService {
@@ -12,7 +13,13 @@ export class AdvertService {
     }
 
     getAdvert(id: string): Observable<Advert> {
-        return this.http.get<Advert>(`/adverts/${id}`);
+        // todo change it and fix it!!!
+        return this.getAdverts().pipe(
+            map(res => {
+                return res.find(advert => advert.id === id);
+            })
+        );
+        // return this.http.get<Advert>(`/adverts/${id}`);
     }
 
     createAdvert(advert: Partial<Advert>): Observable<Advert> {
