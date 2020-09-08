652e5992baea63dd481ec51f9537f60a9c0d98ee
Tim Shenin <t.shenin@celonis.com>
2020-09-08 21:30:57 +0200

    Correct module import

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -7,11 +7,12 @@ import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NgZorroAntdModule } from 'ng-zorro-antd';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzSelectModule } from 'ng-zorro-antd/select';
 import { NzTypographyModule } from 'ng-zorro-antd/typography';
 import { NzSkeletonModule } from 'ng-zorro-antd/skeleton';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { NzCardModule } from 'ng-zorro-antd/card';
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 import { CategoryComponent } from './pages/category/category.component';
@@ -32,7 +33,8 @@ const NZModules = [
     NzSkeletonModule,
     NzInputModule,
     NzSelectModule,
-    NgZorroAntdModule,
+    NzSpinModule,
+    NzCardModule,
 ];
 
 const pages = [MainComponent, CategoryComponent, AdvertComponent, AdvertEditComponent, AdvertCreateComponent, AdvertsListComponent];
