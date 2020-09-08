e4a43e1ab7226462daebda708148d5dd9f4c49ef
Tim Shenin <t.shenin@celonis.com>
2020-09-08 20:57:48 +0200

    Advert page (#30)

--- src/app/modules/main/pages/advert/advert.component.ts
+++ src/app/modules/main/pages/advert/advert.component.ts
@@ -1,12 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { switchMap } from 'rxjs/operators';
+import { AdvertService } from '../../../../shared/services';
+import { ActivatedRoute } from '@angular/router';
+import { Advert } from '../../../../shared/models/models.dto';
+import { Observable } from 'rxjs';
 
 @Component({
     selector: 'app-advert',
     templateUrl: './advert.component.html',
     styleUrls: ['./advert.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AdvertComponent implements OnInit {
-    constructor() {}
+    advert$: Observable<Advert>;
 
-    ngOnInit(): void {}
+    constructor(private advertService: AdvertService, private route: ActivatedRoute, private cd: ChangeDetectorRef) {}
+
+    ngOnInit(): void {
+        this.advert$ = this.route.paramMap.pipe(
+            switchMap(params => {
+                return this.advertService.getAdvert(params.get('advert_id'));
+            })
+        );
+    }
 }
