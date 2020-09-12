b7c7222f89ec0574bc670aaa7ec8132b5e68c903
aradionov <aradionov@phoenixit.ru>
2020-09-12 21:42:45 +0700

    review

--- src/app/modules/main/components/pagination/pagination.component.ts
+++ src/app/modules/main/components/pagination/pagination.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input, ChangeDetectionStrategy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-pagination',
     templateUrl: './pagination.component.html',
     styleUrls: ['./pagination.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PaginationComponent implements OnInit {
     @Output() page = new EventEmitter<string>();
     @Input() totalAdverts: number;
+    @Input() pageSize: number;
     pageIndex = 1;
 
     constructor(private route: ActivatedRoute) {}
