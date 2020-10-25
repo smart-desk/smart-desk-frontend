a4c7a40473e0a66ba70e456811ff37d7de241007
Tim Shenin <t.shenin@celonis.com>
2020-10-25 22:02:01 +0100

    Add staging as environment

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: true,
+    production: false,
     apiURL: 'http://138.197.190.22/api',
     nestApiUrl: 'http://138.197.190.22/api',
     googleClientId: '725037573536-orc0fpben8knki7kfqrl3d734t173t3c.apps.googleusercontent.com',
