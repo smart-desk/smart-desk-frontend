1f9ff01b119058ce3f920afddf54d5fdcf2ae65e
Tim Shenin <t.shenin@celonis.com>
2020-11-16 23:02:04 +0100

    Updated environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
     production: true,
     apiURL: 'http://138.197.190.22/api',
-    nestApiUrl: 'http://138.197.190.22/api',
     googleClientId: '725037573536-orc0fpben8knki7kfqrl3d734t173t3c.apps.googleusercontent.com',
     retry: 0,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
     production: false,
-    apiURL: 'http://localhost:3000/api',
-    nestApiUrl: 'http://localhost:3001/api',
+    apiURL: 'http://localhost:3001/api',
     retry: 0,
     googleClientId: '725037573536-orc0fpben8knki7kfqrl3d734t173t3c.apps.googleusercontent.com',
 };
