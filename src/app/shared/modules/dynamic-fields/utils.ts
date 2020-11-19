b929d6324b300ae76586972be8638e47430b6227
Tim Shenin <t.shenin@celonis.com>
2020-11-20 00:03:44 +0100

    Simplified module usage twice

--- src/app/shared/modules/dynamic-fields/utils.ts
+++ src/app/shared/modules/dynamic-fields/utils.ts
@@ -1,9 +1,9 @@
 import { FieldType } from '../../models/dto/field.entity';
-import { InjectionToken, Provider } from '@angular/core';
+import { InjectionToken, Provider, Type } from '@angular/core';
 import { DynamicFieldsMap } from './dynamic-fields.map';
 import { AbstractFieldService } from './abstract-field.service';
 
-export function createDynamicFieldProvider(type: FieldType, useClass): Provider {
+export function createDynamicFieldProvider(type: FieldType, useClass: Type<AbstractFieldService>): Provider {
     DynamicFieldsMap.set(type, new InjectionToken<AbstractFieldService>(type));
 
     return { provide: DynamicFieldsMap.get(type), useClass };
