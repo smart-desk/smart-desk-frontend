cd171c17179662d18a3cf012cd7567ede2f4ced4
Tim Shenin <t.shenin@celonis.com>
2020-12-08 00:19:43 +0100

    Show thumb in card

--- src/app/modules/main/components/advert-card/advert-card.component.ts
+++ src/app/modules/main/components/advert-card/advert-card.component.ts
@@ -1,5 +1,8 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Advert } from '../../../../shared/models/dto/advert.entity';
+import { SectionType } from '../../../../shared/models/dto/section.entity';
+import { FieldType } from '../../../../shared/models/dto/field.entity';
+import { PhotoEntity } from '../../../../shared/models/dto/field-data/photo.entity';
 
 @Component({
     selector: 'app-advert-card',
@@ -9,4 +12,16 @@ import { Advert } from '../../../../shared/models/dto/advert.entity';
 })
 export class AdvertCardComponent {
     @Input() advert: Advert;
+
+    getThumbSrc(): string {
+        const section = this.advert.sections.find(s => s.type === SectionType.PARAMS);
+        if (!section || !section.fields.length) {
+            return;
+        }
+
+        const photoField = section.fields.find(f => f.type === FieldType.PHOTO);
+        if (photoField) {
+            return (photoField.data as PhotoEntity).value[0];
+        }
+    }
 }
