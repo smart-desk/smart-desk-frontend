038b8b425793cd1bdd1d80f24a6f3de5e3ae6156
Tim Shenin <t.shenin@celonis.com>
2021-01-12 12:07:41 +0100

    Added my adverts (#70)

--- src/app/modules/main/pages/my-adverts/my-adverts.component.ts
+++ src/app/modules/main/pages/my-adverts/my-adverts.component.ts
@@ -1,12 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { AdvertService, UserService } from '../../../../shared/services';
+import { GetAdvertsResponseDto } from '../../../../shared/models/dto/advert.dto';
+import { User } from '../../../../shared/models/dto/user/user.entity';
 
 @Component({
     selector: 'app-my-adverts',
     templateUrl: './my-adverts.component.html',
     styleUrls: ['./my-adverts.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MyAdvertsComponent implements OnInit {
-    constructor() {}
+    adverts: GetAdvertsResponseDto;
+    user: User;
 
-    ngOnInit(): void {}
+    constructor(private advertService: AdvertService, private cdr: ChangeDetectorRef, private userService: UserService) {}
+
+    ngOnInit(): void {
+        this.userService.getCurrentUser().subscribe(res => {
+            this.user = res;
+            this.cdr.detectChanges();
+        });
+
+        this.advertService.getMyAdverts().subscribe(res => {
+            this.adverts = res;
+            this.cdr.detectChanges();
+        });
+    }
 }
