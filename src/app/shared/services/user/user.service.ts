1859ca2cfca6cc8964219bdc77a5e0ba9bd2cfea
Tim Shenin <t.shenin@celonis.com>
2021-01-17 22:16:15 +0100

    Block user button

--- src/app/shared/services/user/user.service.ts
+++ src/app/shared/services/user/user.service.ts
@@ -5,6 +5,7 @@ import { tap } from 'rxjs/operators';
 import { User } from '../../models/dto/user/user.entity';
 import { UpdateUserDto } from '../../models/dto/user/update-user.dto';
 import { UpdateUserRolesDto } from '../../models/dto/user/update-user-roles.dto';
+import { BlockUserDto } from '../../models/dto/user/block-user.dto';
 
 @Injectable({
     providedIn: 'root',
@@ -39,5 +40,9 @@ export class UserService {
 
     updateUserRoles(id: string, body: UpdateUserRolesDto): Observable<User> {
         return this.http.patch<User>(`/users/${id}/roles`, body);
+    }
+
+    blockUser(id: string, value: BlockUserDto): Observable<User> {
+        return this.http.patch<User>(`/users/${id}/block`, value);
     }
 }
