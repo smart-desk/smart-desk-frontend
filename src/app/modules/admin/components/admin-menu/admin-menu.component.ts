24499cfd175f5ac198d8044a324e07c33f5b9574
Tim Shenin <t.shenin@celonis.com>
2021-01-24 22:02:24 +0100

    Added page for pending adverts

--- src/app/modules/admin/components/admin-menu/admin-menu.component.ts
+++ src/app/modules/admin/components/admin-menu/admin-menu.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
     selector: 'app-admin-menu',
     templateUrl: './admin-menu.component.html',
     styleUrls: ['./admin-menu.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AdminMenuComponent {}
