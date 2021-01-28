0c3ed12b4b4d32d98554d5b192eabed9df428312
Tim Shenin <t.shenin@celonis.com>
2021-01-28 21:01:10 +0100

    Added rich text editor as an option (#78)

--- src/app/modules/dynamic-fields/textarea/textarea-view/textarea-view.component.ts
+++ src/app/modules/dynamic-fields/textarea/textarea-view/textarea-view.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, SecurityContext } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { AbstractFieldViewComponent } from '../../../../shared/modules/dynamic-fields/models/abstract-field-view.component';
 import { TextareaEntity } from '../dto/textarea.entity';
 import { TextareaParamsDto } from '../dto/textarea-params.dto';
@@ -9,4 +10,16 @@ import { TextareaParamsDto } from '../dto/textarea-params.dto';
     styleUrls: ['./textarea-view.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TextareaViewComponent extends AbstractFieldViewComponent<TextareaEntity, TextareaParamsDto> {}
+export class TextareaViewComponent extends AbstractFieldViewComponent<TextareaEntity, TextareaParamsDto> implements OnInit {
+    text: string;
+
+    constructor(private sanitizer: DomSanitizer, private cdr: ChangeDetectorRef) {
+        super();
+    }
+
+    ngOnInit() {
+        const value = this.field?.data?.value;
+        this.text = this.sanitizer.sanitize(SecurityContext.HTML, value || '');
+        this.cdr.detectChanges();
+    }
+}
