f98ba03bac0af6a05c26500d680e9e6f8c30d8ba
Tim Shenin <t.shenin@celonis.com>
2021-03-18 00:06:40 +0100

    Saving params and values

--- src/app/modules/dynamic-fields/checkbox/checkbox-form/checkbox-form.component.ts
+++ src/app/modules/dynamic-fields/checkbox/checkbox-form/checkbox-form.component.ts
@@ -1,8 +1,7 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { AbstractFieldFormComponent } from '../../../../shared/modules/dynamic-fields/models/abstract-field-form.component';
 import { CheckboxEntity } from '../dto/checkbox.entity';
-import { CheckboxParamsDto } from '../dto/checkbox-params.dto';
+import { CheckboxItem, CheckboxParamsDto } from '../dto/checkbox-params.dto';
 
 @Component({
     selector: 'app-radio',
@@ -10,29 +9,31 @@ import { CheckboxParamsDto } from '../dto/checkbox-params.dto';
     styleUrls: ['./checkbox-form.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CheckboxFormComponent extends AbstractFieldFormComponent<CheckboxEntity, CheckboxParamsDto> implements OnInit {
-    form: FormGroup;
-
-    ngOnInit(): void {
-        this.form = new FormGroup({
-            value: new FormControl(this.field.data && this.field.data.value),
-        });
-    }
+export class CheckboxFormComponent extends AbstractFieldFormComponent<CheckboxEntity, CheckboxParamsDto> {
+    private currentValue: string[];
 
     getFieldData(): any {
         if (this.field.data) {
-            this.field.data.value = this.form.get('value').value;
+            this.field.data.value = this.currentValue;
             return this.field.data;
         }
 
         const advertField = new CheckboxEntity();
-        advertField.value = this.form.get('value').value;
+        advertField.value = this.currentValue;
         advertField.field_id = this.field.id;
 
         return advertField;
     }
 
     isFieldDataValid(): boolean {
-        return this.form.valid;
+        return true;
+    }
+
+    isChecked(cb: CheckboxItem): boolean {
+        return this.field?.data?.value.some(c => c === cb.value);
+    }
+
+    checkboxChecked(checked: string[]): void {
+        this.currentValue = checked;
     }
 }
