30b811635b2ece1fe696817b4150674b009ce489
Tim Shenin <t.shenin@celonis.com>
2021-03-19 00:09:10 +0100

    Fixed checkbox view component

--- src/app/modules/dynamic-fields/checkbox/checkbox-view/checkbox-view.component.ts
+++ src/app/modules/dynamic-fields/checkbox/checkbox-view/checkbox-view.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { AbstractFieldViewComponent } from '../../../../shared/modules/dynamic-fields/models/abstract-field-view.component';
 import { CheckboxEntity } from '../dto/checkbox.entity';
 import { CheckboxParamsDto } from '../dto/checkbox-params.dto';
@@ -9,4 +9,18 @@ import { CheckboxParamsDto } from '../dto/checkbox-params.dto';
     styleUrls: ['./checkbox-view.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CheckboxViewComponent extends AbstractFieldViewComponent<CheckboxEntity, CheckboxParamsDto> {}
+export class CheckboxViewComponent extends AbstractFieldViewComponent<CheckboxEntity, CheckboxParamsDto> implements OnInit {
+    valueText: string;
+
+    constructor(private cdr: ChangeDetectorRef) {
+        super();
+    }
+
+    ngOnInit() {
+        this.valueText = this.field.params.checkboxes
+            .filter(ch => this.field.data.value.includes(ch.value))
+            .map(ch => ch.label)
+            .join(', ');
+        this.cdr.detectChanges();
+    }
+}
