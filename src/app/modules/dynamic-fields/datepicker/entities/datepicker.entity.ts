2d1c09c8b81c1bb7cb3e393c2065c8554e3f878e
Tim Shenin <t.shenin@celonis.com>
2021-04-28 14:58:23 +0200

    Added dtos and entity

---
+++ src/app/modules/dynamic-fields/datepicker/entities/datepicker.entity.ts
@@ -0,0 +1,6 @@
+import { DynamicFieldsBaseEntity } from '../../../../shared/models/field/dynamic-fields-base.entity';
+
+export class DatepickerEntity extends DynamicFieldsBaseEntity {
+    date1: Date;
+    date2: Date;
+}
