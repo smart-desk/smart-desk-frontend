0d4c9739c11149b2f14a6e3756eba971b1a794f3
Tim Shenin <t.shenin@celonis.com>
2021-06-20 22:34:46 +0200

    Apply some changes

--- src/app/ui-modules/main/pages/profile/profile.module.ts
+++ src/app/ui-modules/main/pages/profile/profile.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './components/profile/profile.component';
 import { NzTableModule } from 'ng-zorro-antd/table';
@@ -8,15 +9,23 @@ import { MyAdvertsComponent } from './components/my-adverts/my-adverts.component
 import { NzTabsModule } from 'ng-zorro-antd/tabs';
 import { MainModule } from '../../main.module';
 import { NotificationsComponent } from './components/notifications/notifications.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
-
-const components = [ProfileComponent, BookmarksComponent, MyAdvertsComponent, NotificationsComponent];
 import { MyAdCampaignsComponent } from './components/my-ad-campaigns/my-ad-campaigns.component';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 
+const components = [ProfileComponent, BookmarksComponent, MyAdvertsComponent, NotificationsComponent, MyAdCampaignsComponent];
+
 @NgModule({
-    declarations: [ProfileComponent, BookmarksComponent, MyAdvertsComponent, MyAdCampaignsComponent],
-    imports: [CommonModule, ProfileRoutingModule, MainModule, NzTableModule, NzTabsModule, NzButtonModule],
+    declarations: [...components],
+    imports: [
+        CommonModule,
+        ProfileRoutingModule,
+        MainModule,
+        NzTableModule,
+        NzTabsModule,
+        NzButtonModule,
+        ReactiveFormsModule,
+        NzCheckboxModule,
+    ],
 })
 export class ProfileModule {}
