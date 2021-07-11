4a8023cdb1cc0e7c77aca060457ef530a6f70fec
Tim Shenin <t.shenin@celonis.com>
2021-07-11 19:39:44 +0200

    ad card refactoring

--- src/app/components/ad-card/ad-card.component.ts
+++ src/app/components/ad-card/ad-card.component.ts
@@ -1,19 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { AdCampaignEntity } from '../../modules/ad/models/ad-campaign.entity';
 
 @Component({
     selector: 'app-ad-card',
     templateUrl: './ad-card.component.html',
     styleUrls: ['./ad-card.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AdCardComponent implements OnInit {
-    thumb = '';
-    link = '';
+export class AdCardComponent {
     @Input() ad: AdCampaignEntity;
-    constructor() {}
-
-    ngOnInit(): void {
-        this.link = this.ad?.link;
-        this.thumb = this.ad?.img;
-    }
 }
