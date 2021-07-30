24a9375ed40a895062dbc2a6ef0adadd5ba859af
Tim Shenin <t.shenin@celonis.com>
2021-07-30 22:34:11 +0200

    Updated to https

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
-    apiURL: 'http://smart-desk.me/api',
+    apiURL: 'https://smart-desk.me/api',
     googleClientId: '725037573536-i5c39a684oubouh7aap1l4mribgkl8fl.apps.googleusercontent.com',
     retry: 0,
     googleMapsApiKey: 'AIzaSyAdJwILlE4YP3369mGiGjqbpwRRnKxRDYI',
-    webSocketUrl: 'http://smart-desk.me',
+    webSocketUrl: 'https://smart-desk.me',
     vkAppId: '7865563',
-    host: 'http://smart-desk.me',
+    host: 'https://smart-desk.me',
     stripePublicKey: 'pk_test_51J0ZFPCUnxSYC4x5Fd8di1tY1lsc4A4J9p90y3VPHSQJEJQl7zYAUP4qUDezVRu6yZzB9r8oZOMZe5THod8e6KgR00dZN8LpYe',
 };
