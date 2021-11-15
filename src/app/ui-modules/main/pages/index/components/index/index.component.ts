14f025c3dc6e2bdf478a142e7b10fde64360292b
Tim Shenin <t.shenin@celonis.com>
2021-11-15 21:39:26 +0100

    Recently added prodcuts

--- src/app/ui-modules/main/pages/index/components/index/index.component.ts
+++ src/app/ui-modules/main/pages/index/components/index/index.component.ts
@@ -2,6 +2,8 @@ import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@
 import { AdCampaignType } from '../../../../../../modules/ad/models/ad-campaign.entity';
 import { AdService } from '../../../../../../modules/ad/ad.service';
 import { AdCampaignCurrentDto } from '../../../../../../modules/ad/models/ad-campaign-current.dto';
+import { GetProductsDto, GetProductsResponseDto } from '../../../../../../modules/product/models/product.dto';
+import { ProductService } from '../../../../../../modules/product/product.service';
 
 @Component({
     selector: 'app-index',
@@ -11,12 +13,20 @@ import { AdCampaignCurrentDto } from '../../../../../../modules/ad/models/ad-cam
 })
 export class IndexComponent implements OnInit {
     mainAdCampaign: AdCampaignCurrentDto;
+    recentProducts: GetProductsResponseDto;
 
-    constructor(private adService: AdService, private cd: ChangeDetectorRef) {}
+    constructor(private adService: AdService, private cd: ChangeDetectorRef, private productService: ProductService) {}
 
     ngOnInit() {
         this.adService.getAdCampaignsCurrent(AdCampaignType.MAIN).subscribe(campaign => {
             this.mainAdCampaign = campaign;
+            this.cd.detectChanges();
+        });
+
+        const options = new GetProductsDto();
+        options.limit = 8;
+        this.productService.getProducts(options).subscribe(res => {
+            this.recentProducts = res;
             this.cd.detectChanges();
         });
     }
