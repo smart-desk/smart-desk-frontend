d73c3c4b0620977efc50c0db17184ca9bd644ba8
Tim Shenin <t.shenin@celonis.com>
2022-01-08 20:19:44 +0100

    Mobile view of a product

--- src/app/ui-modules/main/components/user-info/user-info.component.ts
+++ src/app/ui-modules/main/components/user-info/user-info.component.ts
@@ -4,6 +4,7 @@ import { User } from '../../../../modules/user/models/user.entity';
 @Component({
     selector: 'app-user-info',
     templateUrl: './user-info.component.html',
+    styleUrls: ['./user-info.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserInfoComponent {
